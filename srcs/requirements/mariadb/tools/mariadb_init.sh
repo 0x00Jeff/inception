656ef8c3748c9bffec2e4d4ccfb2bd6779637e20
0x00Jeff <jeff_septiceye@protonmail.com>
2024-09-17 22:43:02 +0100

    Feat: saving shit

--- srcs/requirements/mariadb/tools/mariadb_init.sh
+++ srcs/requirements/mariadb/tools/mariadb_init.sh
@@ -2,9 +2,11 @@
 
 #sleep infinity
 
-mysql_install_db
+sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 /etc/init.d/mariadb start
+
+mysql_install_db
 
 mysql_secure_installation -h localhost << EOF > /dev/null 2>&1
 
@@ -23,9 +25,10 @@ cat <<EOF > db1.sql
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
 FLUSH PRIVILEGES;
 EOF
+
+#SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
 
 # Execute SQL script
 mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -h localhost < db1.sql
---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# configure fpm to listen on the network
+
+FPM_CONFIG="/etc/php/8.2/fpm/pool.d/www.conf"
+sed -i 's|listen = /run/php/php8.2-fpm.sock|listen = 9000|g' "${FPM_CONFIG}"
+
+
+# download wordpress
+wp core download --path=/var/www/html/
+
+# configure access to mariadb
+WORDPRESS_SAMPLE_CONFIG="/var/www/html/wp-config-sample.php"
+WORDPRESS_CONFIG="/var/www/html/wp-config.php"
+
+cp ${WORDPRESS_SAMPLE_CONFIG} ${WORDPRESS_CONFIG}
+
+sed -i "s|database_name_here|${MYSQL_DATABASE}|g" "${WORDPRESS_CONFIG}"
+sed -i "s|username_here|${MYSQL_USER}|g" "${WORDPRESS_CONFIG}"
+sed -i "s|password_here|${MYSQL_PASSWORD}|g" "${WORDPRESS_CONFIG}"
+sed -i "s|localhost|${MYSQL_HOST}|g" "${WORDPRESS_CONFIG}"
+
+
+# install wordpress
+wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --path=/var/www/html --skip-email
+
+# download wordpress theme
+wp theme install codeify --activate --path=/var/www/html
+
+sleep infinity
