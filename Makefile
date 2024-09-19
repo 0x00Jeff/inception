307641decc7e39c498de88bb39702af670f8b7dd
Abdelhamid Fatimi <jeff_septiceye@protonmail.com>
2024-09-19 02:45:12 +0100

    Fix: links, Remove: wordpress theme

--- Makefile
+++ Makefile
@@ -2,16 +2,16 @@ all: build up
 
 build:
 	docker-compose -f ./srcs/docker-compose.yml build
+
 up:
 	docker-compose -f ./srcs/docker-compose.yml up
 
 down:
 	docker-compose -f ./srcs/docker-compose.yml down -v -t 1
-stop:
-	docker-compose -f ./srcs/docker-compose.yml stop -t 1
-clean:
-	docker-compose -f ./srcs/docker-compose.yml down -v -t 1
-prune:
+
+clean: down
+	# TODO : find a better solution for this later!!!
+	rm -rf srcs/data/wordpress/*
 	docker system prune -a -f
 
-re: clean all
+re: down all
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -5,21 +5,20 @@
 FPM_CONFIG="/etc/php/8.2/fpm/pool.d/www.conf"
 sed -i 's|listen = /run/php/php8.2-fpm.sock|listen = 9000|g' "${FPM_CONFIG}"
 
-
 # download wordpress
 wp core download --path=/var/www/html/
 
-# configure access to mariadb
-WORDPRESS_SAMPLE_CONFIG="/var/www/html/wp-config-sample.php"
+# copy config file
 WORDPRESS_CONFIG="/var/www/html/wp-config.php"
+mv /wp-config.php ${WORDPRESS_CONFIG}
 
-cp ${WORDPRESS_SAMPLE_CONFIG} ${WORDPRESS_CONFIG}
-
+# set the right config for wordpress
 sed -i "s|database_name_here|${MYSQL_DATABASE}|g" "${WORDPRESS_CONFIG}"
 sed -i "s|username_here|${MYSQL_USER}|g" "${WORDPRESS_CONFIG}"
 sed -i "s|password_here|${MYSQL_PASSWORD}|g" "${WORDPRESS_CONFIG}"
 sed -i "s|localhost|${MY_MYSQL_HOST}|g" "${WORDPRESS_CONFIG}"
-
+sed -i "s|wp_home_here|${WP_HOME}|g" "${WORDPRESS_CONFIG}"
+sed -i "s|wp_siteurl_here|${WP_SITEURL}|g" "${WORDPRESS_CONFIG}"
 
 # install wordpress
 wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --path=/var/www/html --skip-email
@@ -27,6 +26,4 @@ wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER
 # download wordpress theme
 #wp theme install codeify --activate --path=/var/www/html
 
-service php8.2-fpm start
-
-sleep infinity
+php-fpm8.2 -F
