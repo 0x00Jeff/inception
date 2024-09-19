a5e00f107d227276f66fedc784885bd2fa9e1372
0x00Jeff <jeff_septiceye@protonmail.com>
2024-09-19 19:00:49 +0100

    Fix: plugins issue

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -26,6 +26,7 @@ sed -i "s|wp_siteurl_here|${WP_SITEURL}|g" "${WORDPRESS_CONFIG}"
 wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --path=/var/www/html --skip-email
 wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_PASSWORD} --path=/var/www/html
 
+chown -R www-data:www-data /var/www/html
 
 # download wordpress theme
 #wp theme install codeify --activate --path=/var/www/html
